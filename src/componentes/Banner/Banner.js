bc4a4d7563a2df99a8e6f81051242801031c2f78
Vinicius Costa <viniocosta91@gmail.com>
2026-06-21 17:53:36 -0300

    projeto completo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+            </header>
+            <h1>Teste</h1>
+        </>
+    )
+}
